f0feb14b268756fa96548e465f4dc10cfe792937
KCl <k_nacl@qq.com>
2023-05-08 19:46:15 +0800

    #查询作者精神状态#

--- attack/DeepFool.py
+++ attack/DeepFool.py
@@ -30,58 +30,80 @@ class DeepFool(BaseAttack):
     
     def attack_one(
         self, 
-        img: torch.Tensor,
+        image: torch.Tensor,
         overshoot: float = 0.02,
         num_classes: int = 10,
         max_iter: int = 100,
     ) -> torch.Tensor:
-        img_clone, i = img.clone(), 0
-        img_clone = img_clone.to(self.device)
-        if img_clone.dim() == 3:
-            img_clone.unsqueeze_(0)
-            
-        with torch.no_grad():
-            raw_logits = self.model(img_clone)
-            label = torch.argmax(F.softmax(raw_logits, dim = 1)).item()
-        perturbed_label = label
-        
-        while i < max_iter:
-            grads, fs = [], []
-            self.model.zero_grad()
-            img_clone.requires_grad_(True)
-            if img_clone.grad is not None:
-                img_clone.grad.zero_()
-            logits: torch.Tensor = self.model(img_clone)
-            with torch.no_grad():
-                perturbed_label = F.softmax(logits, dim = 1).argmax().item()
-                # print(f'perturbed_label: {perturbed_label}, label: {label}, i: {i}')
-                if perturbed_label != label:
-                    break
-            logits[0, label].backward(retain_graph = True)
-            grad_raw = img_clone.grad.clone()
-            
-            for k in range(num_classes):
-                if k == label: continue
-                self.model.zero_grad()
-                if img_clone.grad is not None:
-                    img_clone.grad.zero_()
-                logits[0, k].backward(retain_graph = True)
-                grad = img_clone.grad.clone()
-                
-                grads.append(grad)
-                fs.append(logits[0, k].item())
-            
-            tmp1 = (torch.as_tensor(fs) - logits[0, label].item()).to(self.device)
-            tmp2 = (torch.cat(grads, dim = 0) - grad_raw).reshape(9, -1).to(self.device)
-            tmp3: torch.Tensor = torch.abs(tmp1) / torch.linalg.norm(tmp2, dim = 1)
-            l = tmp3.argmin().item()
-            
-            r = tmp3[l] * grads[l]
-            with torch.no_grad():
-                img_clone += r * (1 + overshoot)
-                img_clone = torch.clamp(img_clone, 0, 1)
-            i += 1
-            
-        return img_clone.detach().cpu()
+        """
+        对抗攻击算法 DeepFool 的实现
+        :param image: 原始图像
+        :param net: 用于分类的神经网络
+        :param max_iter: 最大迭代次数
+        :param overshoot: 用于微调原始输入以生成对抗样本的超调系数
+        :return: 对抗样本
+        """
+        # 将图像张量转为可求导的状态，并将其复制为一个新的 tensor，用于存储扰动向量
+        img_tensor = image.clone().detach().to(self.device)
+        img_tensor.unsqueeze_(0)
+        img_tensor.requires_grad_(True)
+        perturbation = torch.zeros_like(img_tensor)
+
+        # 对抗样本分类预测标签和置信度
+        f_image = self.model(img_tensor).data.cpu().numpy().flatten()
+        I = f_image.argsort()[::-1]
+
+        # 最大类别及其分数
+        label0 = I[0]
+        score0 = f_image[label0]
+
+        # 用语句计数器初始化迭代次数和扰动的步数
+        count = 0
+        k = label0
+
+        while k == label0 and count < max_iter:
+            # 计算关于当前样本的梯度
+            input_var = img_tensor.requires_grad_(True)
+            output = self.model(input_var)
+            pred_label = output.data.max(1, keepdim=True)[1][0].item()
+            if pred_label != label0:
+                break
+            grad_f = torch.zeros_like(output)
+            grad_f[0][k] = 1
+            output.backward(gradient=grad_f, retain_graph=True)
+
+            # 计算当前样本到决策边界的距离（即最小扰动的模）
+            w_k = (self.model.fc.weight.data[label0, :] - self.model.fc.weight.data[k, :]).clone().detach().to(self.device)
+            f_k = self.model.fc.bias.data[label0] - self.model.fc.bias.data[k]
+            distance = abs(f_k) / torch.norm(w_k)
+
+            # 将各个标签之间决策边界的线性分隔超平面的法向量进行线性组合
+            pert = torch.zeros_like(self.model.fc.weight.data[0, :]).to(self.device)
+            for i in range(self.model.fc.weight.shape[0]):
+                if i != k:
+                    w_i = torch.tensor(self.model.fc.weight.data[label0, :] - self.model.fc.weight.data[i, :]).to(self.device)
+                    f_i = self.model.fc.bias.data[label0] - self.model.fc.bias.data[i]
+                    pert_i = abs(f_i) / torch.norm(w_i) * w_i
+                    pert = pert + pert_i
+
+            # 计算对抗扰动，保证其尽量小
+            w_norm = torch.norm(pert, p=2)
+
+            r_i = (pert + 1e-4) * torch.ones_like(pert).to(self.device) * pert / w_norm
+            print(r_i.shape)
+            perturbation += r_i
+
+
+            # 对抗攻击计算当前样本的类别
+            img_tensor = img_tensor + (1 + overshoot) * r_i
+            output = self.model(img_tensor)
+            k = output.data.max(1, keepdim=True)[1][0].item()
+
+            count += 1
+
+        # 将图像还原为原始状态，并将扰动裁剪到[-epsilon, epsilon] 区间内
+        img_adv = torch.clamp(img_tensor - image, min=-0.2, max=0.2) + image
+
+        return img_adv.detach().cpu()
             
                 
